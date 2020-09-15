781622a504dca94fd1256f59727b7808eeed4b01
David Dashti <47575784+Dashtid@users.noreply.github.com>
2020-09-15 06:04:40 -0400

    Add files via upload
    yay

--- Seminar 2/Seminar2_Task4.R
+++ Seminar 2/Seminar2_Task4.R
@@ -7,13 +7,15 @@ library(splitstackshape)
 library(tidyr)
 
 # Setting Working Directory
-#setwd("/Den stora KTH mappen/CM2003 - Statistics in Medical Engineering/Seminar 2")      # Laptop
-setwd("/Skola/KTH/CM2009 - Statistics in Medical Engineering/Git/Seminar 2/Data")   # Desktop
+setwd("/Den stora KTH mappen/CM2009 - Statistics in Medical Engineering/Statistics/Seminar 2/Data")      # Laptop
+#setwd("/Skola/KTH/CM2009 - Statistics in Medical Engineering/Git/Seminar 2/Data")   # Desktop
 
 # Reading in kvdata data and attaching the headers to corresponding column
 kvdata = read.csv(file = "kvdata.csv", head = TRUE, sep="")
 attach(kvdata)
 
+kvdata_new = read.csv(file="kvdata.csv", sep="", dec=",", header=TRUE)
+attach(kvdata_new)
 
 # --- Task 4 --- # 
 
@@ -32,168 +34,37 @@ plot(kv,cnr,type="l",
 # Checking how the CNR data fits in a qq-plot
 ggqqplot(cnr)
 
-# Fitting Gaussian dist to CNR values
-normal = fitdistr(cnr, "normal")
-norm = dnorm(x, normal$estimate[1], normal$estimate[2])
+# Fitting to polynomial model
+poly = lm(cnr ~ poly(kv,2), data=kvdata_new)
 
-# Calculating the mean of the data
-mean = mean(cnr)
+predicts = predict(poly)
+predicts_conf =predict(poly, interval="confidence")
+predicts_predict =predict(poly, interval="predict")
 
-# Printing these values
-sprintf("Mean of countrate is: %s", mean)
-sprintf("Mean of Gaussian fit is: %s", normal$estimate[1])
+plot(predict(poly))
 
 
+# Doing plotting of values to visualize
+plot(kv,cnr,type="p",
+     main = "Contrast-to-Noise ratio at different kV settings",
+     xlab = "kV",
+     ylab = "CNR")
+lines(kv,predicts,type="l",col="green")
 
-# Figuring out limits to plot and plotting Gaussian distr.
-summary(kv)
-plot(norm, type = "l")
+summary(poly)
 
-# Creating variables needed for computing the 95 % confidence interval of the Gaussian distribution
-norm1 = qnorm(0.975 ,normal$estimate[1], normal$estimate[2])
-norm2 = qnorm(0.025 ,normal$estimate[1], normal$estimate[2])
+mydata = cbind(kvdata_new, predicts)
+mydata_conf = cbind(kvdata_new, predicts_conf)
+mydata_predict = cbind(kvdata_new, predicts_predict)
 
-# Printing estimates with a 95 % confidence interval
-sprintf("Estimate of countrate w. Gaussian distribution is between: %f and %f", norm2, norm1)
+p = ggplot(mydata, aes(kv, cnr)) + geom_point() #
 
+# Plot intervals
+p + geom_line(aes(y = lwr), color = "red",   data = mydata_predict, linetype = "dashed")+
+        geom_line(aes(y = upr), color = "red",   data = mydata_predict, linetype = "dashed")+
+        geom_ribbon(aes(ymin = lwr, ymax = upr), data = mydata_conf, alpha=0.4) +
+        geom_line(aes(y = fit), data=mydata_conf, color = "blue")
 
+colMax <- function(data) sapply(data, max, na.rm = TRUE)
+colMax(mydata_conf)
 
-
-
-
-
-
-
-
-
-# Calculating the mean of the data
-mean = mean(countRate)
-
-# Printing these values
-sprintf("Mean of countrate is: %s", mean)
-sprintf("Mean of Gaussian fit is: %s", normal$estimate[1])
-sprintf("Lambda of Poisson fit is: %s", poisson$estimate[1])
-
-# --- Task 1 C --- #
-
-# Calculating Poisson and Gaussian distributions
-pois = dpois(time, poisson$estimate[1])
-norm = dnorm(time, normal$estimate[1], normal$estimate[2])
-
-# Creating histogram and plotting it along with Poisson and Gaussian distributions
-hist = hist(countRate,breaks=20,prob=TRUE,
-            main = "Count Rate Experiment",
-            xlab = "Count Rate",
-            ylab = "Density")
-lines(pois,type="p",col="green",lwd=3,)
-lines(norm,type="l",col="red",lwd=3,)
-legend("topright",
-       legend = c("Poisson","Gaussian"),
-       lty=c(1,1),
-       lwd=c(2.5,2.5),
-       col = c("green","red"),
-       title = "Legend")
-
-# --- Task 1 D --- #
-
-# Calculating cumulative distributions of both Poisson and Gaussian for data
-cumulative_pois = ppois(time, poisson$estimate[1], lower.tail = TRUE)
-cumulative_norm = pnorm(time, normal$estimate[1], normal$estimate[2]) 
-
-# Plotting these distributions in a 1x2 figure
-par(mfrow=c(1,3))
-
-# Gaussian
-plot(cumulative_norm,type="l",
-     main = "Cumulative Distributation - Gaussian",
-     xlab = "Count Rate",
-     ylab = "Probability",
-     xlim = c(0,40))
-
-# Poisson
-plot(cumulative_pois,type="l",
-     main = "Cumulative Distributation - Poisson",
-     xlab = "Count Rate",
-     ylab = "Probability",
-     xlim = c(0,40))
-
-# Plotting both on-top of each other
-plot(cumulative_norm,type="l",col="red",
-     main = "Cumulative Distributation - Both",
-     xlab = "Count Rate",
-     ylab = "Probability",
-     xlim = c(0,40))
-lines(cumulative_pois,type="l",col="green")
-legend("bottomright",
-       legend = c("Poisson","Gaussian"),
-       lty=c(1,1),
-       lwd=c(2.5,2.5),
-       col = c("green","red"),
-       title = "Legend")
-
-
-# --- Task 1 E --- #
-
-# Creating variables needed for computing the 95 % confidence interval of the Gaussian distribution
-norm1 = qnorm(0.975 ,normal$estimate[1], normal$estimate[2])
-norm2 = qnorm(0.025 ,normal$estimate[1], normal$estimate[2])
-
-# Creating variables needed for computing the 95 % confidence interval of the Poisson distribution
-pois1 = qpois(0.975, poisson$estimate[1], lower.tail = TRUE)
-pois2 = qpois(0.025, poisson$estimate[1], lower.tail = TRUE)
-
-# Printing estimates with a 95 % confidence interval
-sprintf("Estimate of countrate w. Gaussian distribution is between: %f and %f", norm2, norm1)
-sprintf("Estimate of countrate w. Poisson distribution is between: %f and %f", pois2, pois1)
-
-# --- Task 1 F --- #
-
-# Calculating the probability of getting countrate of 15 cps
-prob_norm = pnorm(15, normal$estimate[1], normal$estimate[2]) 
-prob_pois = ppois(15, poisson$estimate[1], lower.tail = TRUE)
-
-sprintf("Probability of getting 15 cps is: %f with according to Gaussian model", prob_norm)
-sprintf("Probability of getting 15 cps is: %f with according to Poisson model", prob_pois)
-
-# --- Task 1 G --- #
-
-# Chi-squared test to test goodness of the fit
-pois_goodness_data = dpois(11:30, poisson$estimate[1])
-norm_goodness_data = dnorm(11:30, normal$estimate[1],normal$estimate[2])
-
-chisq.test(hist$counts,p=pois_goodness_data, rescale.p=TRUE, simulate.p.value=TRUE)
-chisq.test(hist$counts,p=norm_goodness_data, rescale.p=TRUE, simulate.p.value=TRUE)
-
-# Doing a qq-test to check the performance of fit
-ggqqplot(countRate)
-
-# --- Task 1 H --- #
-
-# Reading in countrate data and attaching the headers to corresponding column
-count_rate_10s = read.table("countrate_data_10s_avg.csv",sep = ";", header=TRUE)
-attach (count_rate_10s)
-
-# Fitting Poisson and Gaussian dist to countrate_data
-poisson_10s = fitdistr(countRate10savg, "Poisson")
-normal_10s = fitdistr(countRate10savg, "normal")
-
-# Random vector to get proper range for dpois/dnorm
-x = 1:500
-
-# Calculating Poisson and Gaussian distributions
-pois_10s = dpois(x, poisson_10s$estimate[1])
-norm_10s = dnorm(x, normal_10s$estimate[1], normal_10s$estimate[2])
-
-# Creating histogram and plotting it along with Poisson and Gaussian distributions
-hist(countRate10savg,breaks=66,prob=TRUE,
-     main = "Count Rate 10s",
-     xlab = "Count Rate",
-     ylab = "Density")
-lines(pois_10s,type="p",col="green",lwd=3,)
-lines(norm_10s,type="l",col="red",lwd=3,)
-legend("topright",
-       legend = c("Poisson","Gaussian"),
-       lty=c(1,1),
-       lwd=c(2.5,2.5),
-       col = c("green","red"),
-       title = "Legend")

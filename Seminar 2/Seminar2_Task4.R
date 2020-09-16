8c578531cfe33e57b63ba08550f0d5917fc3ff64
David Dashti <47575784+Dashtid@users.noreply.github.com>
2020-09-16 11:26:34 -0400

    Add files via upload
    working all day

--- Seminar 2/Seminar2_Task4.R
+++ Seminar 2/Seminar2_Task4.R
@@ -11,60 +11,67 @@ setwd("/Den stora KTH mappen/CM2009 - Statistics in Medical Engineering/Statisti
 #setwd("/Skola/KTH/CM2009 - Statistics in Medical Engineering/Git/Seminar 2/Data")   # Desktop
 
 # Reading in kvdata data and attaching the headers to corresponding column
+
+# --- Alternative 1 --- #
+# Using this method the cnr values will become chr values which needs to be converted to numerical
 kvdata = read.csv(file = "kvdata.csv", head = TRUE, sep="")
 attach(kvdata)
-
-kvdata_new = read.csv(file="kvdata.csv", sep="", dec=",", header=TRUE)
-attach(kvdata_new)
-
-# --- Task 4 --- # 
 
 # Checking the data of both vectors and turning the cnr vector into numerical
 summary(kvdata)
 cnr = as.numeric(gsub(",", ".", cnr))
 
-# Doing plotting of values to visualize
-plot(kv,cnr,type="l",
-     main = "Contrast-to-Noise ratio at different kV settings",
-     xlab = "kV",
-     ylab = "CNR")
+# --- Alternative 2 --- "
+# A much more smoother way of doing the reading of the data which produces two numerical vectors with attach command
+kvdata_new = read.csv(file="kvdata.csv", sep="", dec=",", header=TRUE)
+attach(kvdata_new)
 
-# Just by observing the plot we can see that optimal kV setting is around 80-84 kV
-
-# Checking how the CNR data fits in a qq-plot
-ggqqplot(cnr)
-
-# Fitting to polynomial model
-poly = lm(cnr ~ poly(kv,2), data=kvdata_new)
-
-predicts = predict(poly)
-predicts_conf =predict(poly, interval="confidence")
-predicts_predict =predict(poly, interval="predict")
-
-plot(predict(poly))
-
+# ----------- Task 4 ----------- # 
 
 # Doing plotting of values to visualize
 plot(kv,cnr,type="p",
      main = "Contrast-to-Noise ratio at different kV settings",
      xlab = "kV",
      ylab = "CNR")
-lines(kv,predicts,type="l",col="green")
 
+# Just by observing the plot we can see that optimal kV setting is around 80-84 kV
+
+# Fitting to polynomial model
+poly = lm(cnr ~ poly(kv,2,raw=TRUE), data=kvdata_new)
 summary(poly)
+
+# Creating predictions based on the polynomial model
+predicts = predict(poly)
+predicts_conf = predict(poly, interval="confidence")
+predicts_predict = predict(poly, interval="predict")
+
+# Plotting the predictions
+plot(predicts)
+
+# Doing plotting of values to visualize
+plot(kv,cnr,type="p",
+     main = "Contrast-to-Noise ratio at different kV settings",
+     xlab = "kV",
+     ylab = "CNR")
+lines(kv, predicts, type="l",col="blue")
+summary(poly)
+
 
 mydata = cbind(kvdata_new, predicts)
 mydata_conf = cbind(kvdata_new, predicts_conf)
 mydata_predict = cbind(kvdata_new, predicts_predict)
 
+plot(test)
+        
 p = ggplot(mydata, aes(kv, cnr)) + geom_point() #
 
-# Plot intervals
+# Ploting the intervals
 p + geom_line(aes(y = lwr), color = "red",   data = mydata_predict, linetype = "dashed")+
         geom_line(aes(y = upr), color = "red",   data = mydata_predict, linetype = "dashed")+
         geom_ribbon(aes(ymin = lwr, ymax = upr), data = mydata_conf, alpha=0.4) +
         geom_line(aes(y = fit), data=mydata_conf, color = "blue")
 
-colMax <- function(data) sapply(data, max, na.rm = TRUE)
+# Outputting maximum values of the columns, just for reference purposes
+colMax = function(data) sapply(data, max, na.rm = TRUE)
 colMax(mydata_conf)
 
